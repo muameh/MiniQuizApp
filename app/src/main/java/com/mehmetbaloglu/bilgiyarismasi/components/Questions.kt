a54abf39b9aba7605a78df6f12e0f723361a5654
muameh <balmehmet111@gmail.com>
2024-10-11 10:21:20 +0300

    finalDialogFunc added

--- app/src/main/java/com/mehmetbaloglu/bilgiyarismasi/components/Questions.kt
+++ app/src/main/java/com/mehmetbaloglu/bilgiyarismasi/components/Questions.kt
@@ -1,26 +1,39 @@
 package com.mehmetbaloglu.bilgiyarismasi.components
 
+import android.app.Activity
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonColors
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mehmetbaloglu.bilgiyarismasi.data.model.QuestionItem
 import com.mehmetbaloglu.bilgiyarismasi.ui.viewmodels.QuestionsViewModel
+import com.mehmetbaloglu.bilgiyarismasi.utils.AppColors
 
 
 @Composable
@@ -28,6 +41,9 @@ fun Questions(viewModel: QuestionsViewModel) {
     val questionsList = viewModel.dataInViewModel.value.data?.toMutableList()
     val currentQuestionsIndex = remember { mutableStateOf(0) }
 
+    // Doğru ve yanlış cevap sayacı
+    val correctAnswerCounter = remember { mutableStateOf(0) }
+    val incorrectAnswerCounter = remember { mutableStateOf(0) }
 
     if (viewModel.dataInViewModel.value.loading == true) {
         Column(
@@ -40,7 +56,10 @@ fun Questions(viewModel: QuestionsViewModel) {
     } else {
         if (questionsList != null) {
             if (currentQuestionsIndex.value >= questionsList.size) {
-                //sorular bittiğinde burası çalışır, sonuç ekranına yönlendirilebilir
+                FinalDialog(
+                    correctCount = correctAnswerCounter.value,
+                    incorrectCount = incorrectAnswerCounter.value
+                )
             } else {
                 val question = questionsList[currentQuestionsIndex.value]
                 QuestionDisplay(
@@ -48,6 +67,8 @@ fun Questions(viewModel: QuestionsViewModel) {
                     currentQuestionIndex = currentQuestionsIndex,
                     onNextClicked = { currentQuestionsIndex.value += 1 },
                     onFinalClicked = {},
+                    correctAnswerCounter = correctAnswerCounter, // Doğru sayısını ilet
+                    incorrectAnswerCounter = incorrectAnswerCounter, // Yanlış sayısını ilet
                     viewModel
                 )
             }
@@ -58,7 +79,12 @@ fun Questions(viewModel: QuestionsViewModel) {
 @Composable
 fun QuestionTracker(counter: Int = 1, outOf: Int = 100) {
     Spacer(modifier = Modifier.height(30.dp))
-    Text(text = "Soru $counter / $outOf", modifier = Modifier.padding(8.dp))
+    Text(
+        text = "Soru $counter / $outOf",
+        modifier = Modifier.padding(10.dp),
+        color = AppColors.white,
+        fontSize = 32.sp
+    )
 }
 
 @Composable
@@ -67,6 +93,8 @@ fun QuestionDisplay(
     currentQuestionIndex: MutableState<Int>,
     onNextClicked: (Int) -> Unit = {},
     onFinalClicked: () -> Unit = {},
+    correctAnswerCounter: MutableState<Int>, // Doğru cevap sayacını parametre olarak al
+    incorrectAnswerCounter: MutableState<Int>, // Yanlış cevap sayacını parametre olarak al
     viewModel: QuestionsViewModel
 ) {
     val correctAnswerState = remember(question) {
@@ -77,89 +105,200 @@ fun QuestionDisplay(
         mutableIntStateOf(-1)
     }
 
-    val chociesList = remember(question) {
+    val choicesList = remember(question) {
         question.choices?.toMutableList()
     }
 
     val showResult = remember { mutableStateOf<Boolean?>(null) }
     val resultMessage = remember { mutableStateOf("") }
 
-    // Doğru cevap sayısını tutacak state
-    val correctAnswerCounter = remember { mutableStateOf(0) }
-
     // Sorunun daha önce yanıtlanıp yanıtlanmadığını kontrol eden durum
     val isAnswered = remember { mutableStateOf(false) }
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.Start
+    val context = LocalContext.current // Mevcut Context'i al
+
+    Surface(
+        modifier = Modifier.fillMaxSize(), color = AppColors.back_ground_color
     ) {
-        QuestionTracker(
-            (currentQuestionIndex.value + 1), viewModel.dataInViewModel.value.data?.size ?: 0
-        )
-        Spacer(modifier = Modifier.height(20.dp))
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start,
 
-
-        Column {
-            Text(
-                text = question.question.toString(), modifier = Modifier.padding(8.dp)
+            ) {
+            QuestionTracker(
+                currentQuestionIndex.value + 1, viewModel.dataInViewModel.value.data?.size ?: 0
             )
-            Spacer(modifier = Modifier.height(30.dp))
-            chociesList?.forEachIndexed { index, choice ->
-                Row(
-                    modifier = Modifier.height(30.dp),
-                    verticalAlignment = Alignment.CenterVertically
-
-                ) {
-                    RadioButton(selected = selectedChoiceIndex.intValue == index, onClick = {
-                        if (!isAnswered.value) {
-                            selectedChoiceIndex.intValue = index
-                            correctAnswerState.value =
-                                (selectedChoiceIndex.intValue == question.answers_index)
-
-                            // Doğru cevap sayısını güncelle
-                            if (correctAnswerState.value == true) {
-                                correctAnswerCounter.value += 1
-                            }
-
-                            // Sonuç mesajını ayarla
-                            resultMessage.value = if (correctAnswerState.value == true) {
-                                "Tebrikler! Doğru cevabı bildiniz."
-                            } else {
-                                "Yanlış cevap! Doğru cevap: ${question.answer}"
-                            }
-
-                            showResult.value = true
-                            isAnswered.value = true // Soruyu daha önce cevaplanmış olarak işaretle
-                        }
-
-
-                    })
-                    Text(text = choice.toString())
-                }
-            }
-
-            Button(modifier = Modifier.padding(8.dp), onClick = {
-                onNextClicked(currentQuestionIndex.value)
-                showResult.value = false // Sonuç gösterimini sıfırla
-                isAnswered.value = false // Sonraki soru için yanıt durumunu sıfırla
-            }) {
-                Text(text = "Sonraki")
-            }
-
-            // Sonucu göster
-            if (showResult.value == true) {
-                Text(text = resultMessage.value, modifier = Modifier.padding(8.dp))
+            Spacer(modifier = Modifier.height(20.dp))
+            HorizontalDivider()
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.Start,
+            ) {
                 Text(
-                    text = "Doğru cevap sayınız: ${correctAnswerCounter.value}/${currentQuestionIndex.value + 1}",
-                    modifier = Modifier.padding(8.dp)
+                    text = question.question.toString(),
+                    color = AppColors.white,
+                    modifier = Modifier.padding(12.dp),
+                    fontSize = 24.sp
+
                 )
+                Spacer(modifier = Modifier.height(20.dp))
+                choicesList?.forEachIndexed { index, choice ->
+                    Row(
+                        modifier = Modifier.height(32.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = selectedChoiceIndex.intValue == index,
+                            colors = RadioButtonColors(
+                                selectedColor = if (correctAnswerState.value == true) Color.Green else Color.Red,
+                                unselectedColor = AppColors.white,
+                                disabledSelectedColor = AppColors.white,
+                                disabledUnselectedColor = AppColors.white
+                            ),
+                            onClick = {
+                                handleAnswerSelection(
+                                    index,
+                                    question,
+                                    correctAnswerCounter,
+                                    incorrectAnswerCounter,
+                                    correctAnswerState,
+                                    resultMessage,
+                                    showResult,
+                                    isAnswered,
+                                    selectedChoiceIndex,
+                                    context
+                                )
+                            }
+                        )
+                        Text(
+                            text = choice.toString(),
+                            color = AppColors.white,
+                            fontSize = 18.sp,
+                            modifier = Modifier.clickable {
+                                handleAnswerSelection(
+                                    index,
+                                    question,
+                                    correctAnswerCounter,
+                                    incorrectAnswerCounter,
+                                    correctAnswerState,
+                                    resultMessage,
+                                    showResult,
+                                    isAnswered,
+                                    selectedChoiceIndex,
+                                    context
+                                )
+                            }
+
+                        )
+                    }
+                }
+
+                Button(modifier = Modifier
+                    .padding(8.dp, 8.dp, 8.dp, 18.dp)
+                    .align(Alignment.CenterHorizontally), onClick = {
+                    onNextClicked(currentQuestionIndex.value)
+                    showResult.value = false // Sonuç gösterimini sıfırla
+                    isAnswered.value = false // Sonraki soru için yanıt durumunu sıfırla
+                }) {
+                    Text(text = "Sonraki")
+                }
+
+                // Sonucu göster
+                if (showResult.value == true) {
+                    Column(
+                        modifier = Modifier.fillMaxWidth(), // Genişliği doldur
+                        horizontalAlignment = Alignment.CenterHorizontally, // Yatayda ortala
+                        verticalArrangement = Arrangement.Center // Dikeyde ortala (isteğe bağlı)
+                    ) {
+                        Text(
+                            text = resultMessage.value,
+                            color = if (resultMessage.value.startsWith("T")) Color.Green else Color.Red,
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
+                        Text(
+                            text = "Doğru cevap sayınız: ${correctAnswerCounter.value}/${currentQuestionIndex.value + 1}",
+                            color = AppColors.golden,
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
+                    }
+                }
             }
         }
     }
-
 }
+
+@Composable
+fun FinalDialog(correctCount: Int, incorrectCount: Int) {
+    val context = LocalContext.current // Get the current context
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Quiz Tamamlandı!", fontSize = 24.sp)
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Doğru Sayınız: $correctCount", fontSize = 18.sp)
+            Text(text = "Yanlış Sayınız: $incorrectCount", fontSize = 18.sp)
+
+            Spacer(modifier = Modifier.height(24.dp)) // Add some space
+
+            // Button to close the application
+            Button(onClick = {
+                // Finish the activity
+                (context as? Activity)?.finish()
+            }) {
+                Text(text = "Çıkış Yap") // Button text
+            }
+        }
+    }
+}
+
+
+private fun handleAnswerSelection(
+    index: Int,
+    question: QuestionItem,
+    correctAnswerCounter: MutableState<Int>,
+    incorrectAnswerCounter: MutableState<Int>,
+    correctAnswerState: MutableState<Boolean?>,
+    resultMessage: MutableState<String>,
+    showResult: MutableState<Boolean?>,
+    isAnswered: MutableState<Boolean>,
+    selectedChoiceIndex: MutableIntState, // Yeni parametre olarak ekledik
+    context: Context
+) {
+    if (!isAnswered.value) {
+        selectedChoiceIndex.intValue = index
+        correctAnswerState.value = (selectedChoiceIndex.intValue == question.answers_index)
+
+        // Doğru veya yanlış cevabını say
+        if (correctAnswerState.value == true) {
+            correctAnswerCounter.value += 1 // Doğru sayısını artır
+        } else {
+            incorrectAnswerCounter.value += 1 // Yanlış sayısını artır
+        }
+
+        // Sonuç mesajını ayarla
+        resultMessage.value = if (correctAnswerState.value == true) {
+            "Tebrikler! Doğru cevabı bildiniz."
+        } else {
+            "Yanlış cevap! Doğru cevap: ${question.answer}"
+        }
+
+        showResult.value = true
+        isAnswered.value = true // Soruyu daha önce cevaplanmış olarak işaretle
+    } else {
+        Toast.makeText(
+            context,
+            "Bu soru daha önce cevaplanmıştır.",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+}
+
 
 
 
---
+++ app/src/main/java/com/mehmetbaloglu/bilgiyarismasi/utils/AppColors.kt
@@ -0,0 +1,14 @@
+package com.mehmetbaloglu.bilgiyarismasi.utils
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+
+    val back_ground_color = Color(0xFF001F3F)  // Koyu Mavi
+    val golden = Color(0xFFFFEB55)              // Altın Sarısı
+    val main_blue = Color(0xFF0072B8)           // Ana Mavi
+    val white = Color(0xFFFFFFFF)                // Beyaz
+    val light_gray = Color(0xFFD9D9D9)           // Açık Gri
+
+
+}
